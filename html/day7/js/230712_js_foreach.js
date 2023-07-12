263eb0453f0108d2ad3d8a5e0c3cd25e7d8fd503
thbrthbr <tarzenezrat@gmail.com>
2023-07-12 20:14:59 +0900

    day6 v2

---
+++ html/day7/js/230712_js_foreach.js
@@ -0,0 +1,16 @@
+// let fruits = ["apple", "banana", "orange", "grape"]
+
+// // 원소 인덱스 배열 순서
+// fruits.forEach((data, index, arr) => {
+//     console.log(data);
+//     console.log(index);
+//     console.log(arr);
+// })
+
+// fruits.forEach((num) => {
+
+// })
+
+let str = "Happy day~! ";
+console.log(str.length);
+console.log(str.toUpperCase());
