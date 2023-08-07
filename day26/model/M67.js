570a75df1882fad2443e0156ed517621f11e3d8d
thbrthbr <tarzenezrat@gmail.com>
2023-08-07 17:08:28 +0900

    day26 v1

---
+++ day26/model/M67.js
@@ -0,0 +1,8 @@
+exports.idpw = () => {
+  return [
+    {
+      id: 'id1',
+      pw: 1234,
+    },
+  ]
+}
---
+++ day26/model/MComment.js
@@ -0,0 +1,28 @@
+exports.comments = () => {
+  return [
+    {
+      id: 1,
+      userId: 'helloworld',
+      date: '2023-08-01',
+      comment: '안녕하세요',
+    },
+    {
+      id: 2,
+      userId: 'happy',
+      date: '2023-08-02',
+      comment: '반가워요',
+    },
+    {
+      id: 3,
+      userId: 'lucky',
+      date: '2023-08-03',
+      comment: '행복하세요',
+    },
+    {
+      id: 4,
+      userId: 'good',
+      date: '2023-08-04',
+      comment: '재밌으세요',
+    },
+  ]
+}
